d7e4549fda719010f8319a76402f910d1b3b021f
Strongholder13 <108480254+Strongholder13@users.noreply.github.com>
2022-09-16 20:38:06 +0300

    Update CarDaolmp.java

--- 2.2.1/PP_2_2_1_spring_hibernate2/2.2.1. spring_hibernate/src/main/java/hiber/dao/CarDaolmp.java
+++ 2.2.1/PP_2_2_1_spring_hibernate2/2.2.1. spring_hibernate/src/main/java/hiber/dao/CarDaolmp.java
@@ -12,17 +12,22 @@ import java.util.List;
 
 @Repository
 public class CarDaolmp implements CarDao {
-
     @Autowired
+    public CarDaolmp(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
     private SessionFactory sessionFactory;
+
     @Override
     public void add(Car car) {
         sessionFactory.getCurrentSession().save(car);
     }
+
     @Override
     @SuppressWarnings("unchecked")
     public List<Car> listCars() {
-        TypedQuery<Car> query=sessionFactory.getCurrentSession().createQuery("from Car");
+        TypedQuery<Car> query = sessionFactory.getCurrentSession().createQuery("from Car");
         return query.getResultList();
     }
 }
