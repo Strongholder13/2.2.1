5fb09451eabe6a8f9bc1902d4768030e2c2003e2
Strongholder13 <108480254+Strongholder13@users.noreply.github.com>
2022-09-16 20:39:22 +0300

    Update MainApp.java

--- 2.2.1/PP_2_2_1_spring_hibernate2/2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1/PP_2_2_1_spring_hibernate2/2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,20 +1,7 @@
-package hiber;
-
-import hiber.config.AppConfig;
-import hiber.model.Car;
-import hiber.model.User;
-import hiber.service.CarService;
-import hiber.service.CarServicelmp;
-import hiber.service.UserService;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.sql.SQLException;
-import java.util.List;
-
 public class MainApp {
    public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+      AnnotationConfigApplicationContext context =
+              new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
 
