80a62c5cd2d160b8f8b6438f5792b1d01d1b5e4a
Strongholder13 <108480254+Strongholder13@users.noreply.github.com>
2022-09-23 15:38:57 +0300

    отредактирвал запросы HQL4

--- 2.2.1/PP_2_2_1_spring_hibernate2/2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1/PP_2_2_1_spring_hibernate2/2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -26,15 +26,18 @@ public class UserDaoImp implements UserDao {
 
     @Override
     public List<User> listUsers() {
-        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("FROM User");
+        Query query = sessionFactory.getCurrentSession().createQuery("FROM User");
 
-        List list = sessionFactory.getCurrentSession().createQuery("FROM User").getResultList();
+
+        List list = sessionFactory.getCurrentSession().createQuery("FROM User.class").getResultList();
 
         return query.getResultList();
     }
     public User getFrom(String model, int series){
-        Query<User> query = sessionFactory.getCurrentSession().createQuery("FROM User where car.model =:model and car.series =:series");
+        String qstring = "FROM User where car.model =:model and car.series =:series";
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(qstring, User.class);
         query.setParameter("model", model).setParameter("series", series);
+
         return query.setMaxResults(1).getSingleResult();
     }
 }
